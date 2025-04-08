acd92ff840d279d9a0b040de192d01b7dad58ffc
Julia Neubauer <julia98neubauer@gmail.com>
2025-04-08 13:09:02 +0000

    fix stack card html and change deleteForm cancel reroute

--- src/Form/SodaScsStackDeleteForm.php
+++ src/Form/SodaScsStackDeleteForm.php
@@ -66,14 +66,15 @@ class SodaScsStackDeleteForm extends ContentEntityDeleteForm {
    * {@inheritdoc}
    */
   public function getQuestion() {
-    return $this->t('Are you sure you want to delete component: @label?', ['@label' => $this->entity->label()]);
+    return $this->t('Are you sure you want to delete stack: @label?', ['@label' => $this->entity->label()]);
   }
 
   /**
    * {@inheritdoc}
    */
   public function getCancelUrl() {
-    return new Url('soda_scs_manager.desk');
+    //TODO: Fix "page not found"
+    return Url::fromRoute('entity.soda_scs_stack.canonical', ['soda_scs_stack' => $this->entity->id()]);
   }
 
   /**
