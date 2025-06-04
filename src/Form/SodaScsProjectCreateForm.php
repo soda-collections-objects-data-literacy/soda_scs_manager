f24b118fcc6d0fbb76256e7bc60fc8134219203b
rnsrk <robert@nasarek.org>
2025-06-04 08:21:27 +0200

    add project members to keycloak

--- src/Form/SodaScsProjectCreateForm.php
+++ src/Form/SodaScsProjectCreateForm.php
@@ -10,10 +10,15 @@ use Drupal\Core\Entity\EntityRepositoryInterface;
 use Drupal\Core\Entity\EntityTypeBundleInfoInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Logger\LoggerChannelFactoryInterface;
+use Drupal\Core\Logger\LoggerChannelInterface;
 use Drupal\Core\Session\AccountProxyInterface;
+use Drupal\Core\Utility\Error;
 use Drupal\soda_scs_manager\ComponentActions\SodaScsComponentActionsInterface;
+use Drupal\soda_scs_manager\ServiceActions\SodaScsServiceActionsInterface;
 use Drupal\soda_scs_manager\RequestActions\SodaScsServiceRequestInterface;
 use Drupal\user\EntityOwnerTrait;
+use Psr\Log\LogLevel;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -49,11 +54,32 @@ class SodaScsProjectCreateForm extends ContentEntityForm {
   protected AccountProxyInterface $currentUser;
 
   /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * The logger.
+   *
+   * @var \Drupal\Core\Logger\LoggerChannelInterface
+   */
+  protected LoggerChannelInterface $logger;
+
+  /**
    * The settings config.
    *
    * @var \Drupal\Core\Config\Config
    */
   protected Config $settings;
+
+  /**
+   * The Soda SCS API Actions service.
+   *
+   * @var \Drupal\soda_scs_manager\ComponentActions\SodaScsComponentActionsInterface
+   */
+  protected SodaScsComponentActionsInterface $sodaScsComponentActions;
 
   /**
    * The Soda SCS API Actions service.
@@ -84,18 +110,11 @@ class SodaScsProjectCreateForm extends ContentEntityForm {
   protected SodaScsServiceRequestInterface $sodaScsKeycloakServiceUserActions;
 
   /**
-   * The Soda SCS API Actions service.
+   * The Soda SCS SQL Service Actions service.
    *
-   * @var \Drupal\soda_scs_manager\ComponentActions\SodaScsComponentActionsInterface
+   * @var \Drupal\soda_scs_manager\ServiceActions\SodaScsServiceSqlServiceActionInterface
    */
-  protected SodaScsComponentActionsInterface $sodaScsComponentActions;
-
-  /**
-   * The entity type manager.
-   *
-   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
-   */
-  protected $entityTypeManager;
+  protected SodaScsServiceActionsInterface $sodaScsSqlServiceActions;
 
   /**
    * Constructs a new SodaScsComponentCreateForm.
@@ -110,6 +129,8 @@ class SodaScsProjectCreateForm extends ContentEntityForm {
    *   The entity type bundle info.
    * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entityTypeManager
    *   The entity type manager.
+   * @param \Drupal\Core\Logger\LoggerChannelFactoryInterface $loggerFactory
+   *   The logger factory.
    * @param \Drupal\soda_scs_manager\ComponentActions\SodaScsComponentActionsInterface $sodaScsComponentActions
    *   The Soda SCS API Actions service.
    * @param \Drupal\soda_scs_manager\RequestActions\SodaScsServiceRequestInterface $sodaScsDockerRegistryServiceActions
@@ -120,6 +141,8 @@ class SodaScsProjectCreateForm extends ContentEntityForm {
    *   The Soda SCS API Actions service.
    * @param \Drupal\soda_scs_manager\RequestActions\SodaScsServiceRequestInterface $sodaScsKeycloakServiceUserActions
    *   The Soda SCS API Actions service.
+   * @param \Drupal\soda_scs_manager\ServiceActions\SodaScsServiceActionsInterface $sodaScsSqlServiceActions
+   *   The Soda SCS SQL Service Actions service.
    * @param \Drupal\Component\Datetime\TimeInterface $time
    *   The time service.
    */
@@ -129,22 +152,26 @@ class SodaScsProjectCreateForm extends ContentEntityForm {
     EntityRepositoryInterface $entity_repository,
     EntityTypeBundleInfoInterface $entity_type_bundle_info,
     EntityTypeManagerInterface $entityTypeManager,
+    LoggerChannelFactoryInterface $loggerFactory,
     SodaScsComponentActionsInterface $sodaScsComponentActions,
     SodaScsServiceRequestInterface $sodaScsDockerRegistryServiceActions,
     SodaScsServiceRequestInterface $sodaScsKeycloakServiceClientActions,
     SodaScsServiceRequestInterface $sodaScsKeycloakServiceGroupActions,
     SodaScsServiceRequestInterface $sodaScsKeycloakServiceUserActions,
+    SodaScsServiceActionsInterface $sodaScsSqlServiceActions,
     TimeInterface $time,
   ) {
     parent::__construct($entity_repository, $entity_type_bundle_info, $time);
     $this->currentUser = $currentUser;
     $this->entityTypeManager = $entityTypeManager;
     $this->settings = $configFactory->getEditable('soda_scs_manager.settings');
+    $this->logger = $loggerFactory->get('soda_scs_manager');
     $this->sodaScsComponentActions = $sodaScsComponentActions;
     $this->sodaScsDockerRegistryServiceActions = $sodaScsDockerRegistryServiceActions;
     $this->sodaScsKeycloakServiceClientActions = $sodaScsKeycloakServiceClientActions;
     $this->sodaScsKeycloakServiceGroupActions = $sodaScsKeycloakServiceGroupActions;
     $this->sodaScsKeycloakServiceUserActions = $sodaScsKeycloakServiceUserActions;
+    $this->sodaScsSqlServiceActions = $sodaScsSqlServiceActions;
   }
 
   /**
@@ -157,11 +184,13 @@ class SodaScsProjectCreateForm extends ContentEntityForm {
       $container->get('entity.repository'),
       $container->get('entity_type.bundle.info'),
       $container->get('entity_type.manager'),
+      $container->get('logger.factory'),
       $container->get('soda_scs_manager.component.actions'),
       $container->get('soda_scs_manager.docker_registry_service.actions'),
       $container->get('soda_scs_manager.keycloak_service.client.actions'),
       $container->get('soda_scs_manager.keycloak_service.group.actions'),
       $container->get('soda_scs_manager.keycloak_service.user.actions'),
+      $container->get('soda_scs_manager.sql_service.actions'),
       $container->get('datetime.time'),
     );
   }
@@ -244,89 +273,179 @@ class SodaScsProjectCreateForm extends ContentEntityForm {
         '@machine_name' => $machineName,
       ]));
     }
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function submitForm(array &$form, FormStateInterface $form_state) {
-    #parent::submitForm($form, $form_state);
-
     /* @todo Move this to a separate service. */
+    // Assign rights to all components...
     foreach ($form_state->getValue('connectedComponents') as $componentId) {
+      /** @var \Drupal\soda_scs_manager\Entity\SodaScsComponent $component */
       $component = $this->entityTypeManager->getStorage('soda_scs_component')->load($componentId['target_id']);
-      switch ($component->bundle()) {
-        case 'soda_scs_wisski_component':
-          $wisskiComponent = $component;
-          $wisskiMachineName = $wisskiComponent->machineName->value;
-          // Add group to keycloak user.
-          $keycloakBuildTokenRequest = $this->sodaScsKeycloakServiceClientActions->buildTokenRequest([]);
-          $keycloakMakeTokenRequest = $this->sodaScsKeycloakServiceClientActions->makeRequest($keycloakBuildTokenRequest);
-          if (!$keycloakMakeTokenRequest['success']) {
-            throw new \Exception('Keycloak token request failed.');
-          }
-          $keycloakTokenResponseContents = json_decode($keycloakMakeTokenRequest['data']['keycloakResponse']->getBody()->getContents(), TRUE);
-          $keycloakToken = $keycloakTokenResponseContents['access_token'];
+      // ...and members mentioned in the form.
+      $members = $form_state->getValue('members');
+      foreach ($members as $memberId) {
+        if (!is_array($memberId) || !isset($memberId['target_id'])) {
+          continue;
+        }
+        /** @var \Drupal\user\Entity\User $member */
+        $member = $this->entityTypeManager->getStorage('user')->load($memberId['target_id']);
+        switch ($component->bundle()) {
+          case 'soda_scs_wisski_component':
+            $wisskiComponent = $component;
+            $wisskiMachineName = $wisskiComponent->machineName->value;
+            // Add group to keycloak user.
+            // First get a token for the Keycloak service client.
+            $keycloakBuildTokenRequest = $this->sodaScsKeycloakServiceClientActions->buildTokenRequest([]);
+            $keycloakMakeTokenRequest = $this->sodaScsKeycloakServiceClientActions->makeRequest($keycloakBuildTokenRequest);
+            if (!$keycloakMakeTokenRequest['success']) {
+              Error::logException(
+                $this->logger,
+                new \Exception('Keycloak token request failed.'),
+                'Keycloak token request failed. @message',
+                ['@message' => $keycloakMakeTokenRequest['error']],
+                LogLevel::ERROR
+              );
+              $form_state->setErrorByName('connectedComponents', $this->t('Keycloak token request failed. See logs for more details.'));
+              continue;
+            }
+            $keycloakTokenResponseContents = json_decode($keycloakMakeTokenRequest['data']['keycloakResponse']->getBody()->getContents(), TRUE);
+            $keycloakToken = $keycloakTokenResponseContents['access_token'];
 
-          $keycloakWisskiInstanceUserGroupName = $wisskiMachineName . '-user';
+            // Give rigths to the group members.
+            // 1. We need the UUID of the WissKI User Instance Group .
+            // Build the name of the group to add the user to.
+            $keycloakWisskiInstanceUserGroupName = $wisskiMachineName . '-user';
 
-          // Get all groups from Keycloak for group ids.
-          $keycloakBuildGetAllGroupsRequest = $this->sodaScsKeycloakServiceGroupActions->buildGetAllRequest([
-            'token' => $keycloakToken,
-          ]);
-          $keycloakMakeGetAllGroupsResponse = $this->sodaScsKeycloakServiceGroupActions->makeRequest($keycloakBuildGetAllGroupsRequest);
+            // Get all groups from Keycloak for group ids.
+            $keycloakBuildGetAllGroupsRequest = $this->sodaScsKeycloakServiceGroupActions->buildGetAllRequest([
+              'token' => $keycloakToken,
+            ]);
+            $keycloakMakeGetAllGroupsResponse = $this->sodaScsKeycloakServiceGroupActions->makeRequest($keycloakBuildGetAllGroupsRequest);
 
-          if ($keycloakMakeGetAllGroupsResponse['success']) {
+            if (!$keycloakMakeGetAllGroupsResponse['success']) {
+              Error::logException(
+                $this->logger,
+                new \Exception('Keycloak get all groups request failed.'),
+                'Keycloak get all groups request failed. @message',
+                ['@message' => $keycloakMakeGetAllGroupsResponse['error']],
+                LogLevel::ERROR
+              );
+              $form_state->setErrorByName('connectedComponents', $this->t('Keycloak get all groups request failed. See logs for more details.'));
+              continue;
+            }
+
             $keycloakGroups = json_decode($keycloakMakeGetAllGroupsResponse['data']['keycloakResponse']->getBody()->getContents(), TRUE);
             // Get the admin group id of the WissKI instance.
             $keycloakWisskiInstanceUserGroup = array_filter($keycloakGroups, function ($group) use ($keycloakWisskiInstanceUserGroupName) {
               return $group['name'] === $keycloakWisskiInstanceUserGroupName;
             });
-            $keycloakWisskiInstanceAdminGroup = reset($keycloakWisskiInstanceUserGroup);
-          }
+            $keycloakWisskiInstanceUserGroup = reset($keycloakWisskiInstanceUserGroup);
 
-          // Set up parameters to search for the keycloak user.
-          $getUserParams = [
-            'token' => $keycloakToken,
-            'queryParams' => [
-              'username' => $wisskiComponent->getOwner()->getDisplayName(),
-            ],
-          ];
+            // 2. We need the UUID of the keycloak user.
+            // Set up parameters to search for the keycloak user.
+            $getUserParams = [
+              'token' => $keycloakToken,
+              'queryParams' => [
+                'username' => $member->getDisplayName(),
+              ],
+            ];
 
-          // Get the user from Keycloak via getAllUsers,
-          // because wie do not have the uuid, but only the username.
-          $getAllUsersRequest = $this->sodaScsKeycloakServiceUserActions->buildGetAllRequest($getUserParams);
-          $getAllUsersResponse = $this->sodaScsKeycloakServiceUserActions->makeRequest($getAllUsersRequest);
+            // Get the user from Keycloak via getAllUsers,
+            // because wie do not have the uuid, but only the username.
+            $getAllUsersRequest = $this->sodaScsKeycloakServiceUserActions->buildGetAllRequest($getUserParams);
+            $getAllUsersResponse = $this->sodaScsKeycloakServiceUserActions->makeRequest($getAllUsersRequest);
 
-          if ($getAllUsersResponse['success']) {
+            if (!$getAllUsersResponse['success']) {
+              Error::logException(
+                $this->logger,
+                new \Exception('Keycloak get all users request failed.'),
+                'Keycloak get all users request failed. @message',
+                ['@message' => $getAllUsersResponse['error']],
+                LogLevel::ERROR
+              );
+              $form_state->setErrorByName('connectedComponents', $this->t('Keycloak get all users request failed. See logs for more details.'));
+              continue;
+            }
+
             $allUserData = json_decode($getAllUsersResponse['data']['keycloakResponse']->getBody()->getContents(), TRUE);
-
             // Extract the UUID if user is found.
             if (!empty($allUserData) && is_array($allUserData) && count($allUserData) > 0) {
               $userData = $allUserData[0];
             }
-          }
+            else {
+              Error::logException(
+                $this->logger,
+                new \Exception('Keycloak user not found.'),
+                'Keycloak user @username not found.',
+                ['@username' => $member->getDisplayName()],
+                LogLevel::ERROR
+              );
+              $form_state->setErrorByName(
+                'connectedComponents',
+                $this->t('Keycloak user not found. See logs for more details.'),
+              );
+              continue;
+            }
 
-          // Add user to admin group.
-          $keycloakBuildAddUserToGroupRequest = $this->sodaScsKeycloakServiceUserActions->buildUpdateRequest([
-            'type' => 'group',
-            'routeParams' => [
-              'userId' => $userData['id'],
-              'groupId' => $keycloakWisskiInstanceAdminGroup['id'],
-            ],
-            'token' => $keycloakToken,
-          ]);
-          $keycloakMakeAddUserToGroupRequest = $this->sodaScsKeycloakServiceUserActions->makeRequest($keycloakBuildAddUserToGroupRequest);
+            // 3. Add user to admin group.
+            $keycloakBuildAddUserToGroupRequest = $this->sodaScsKeycloakServiceUserActions->buildUpdateRequest([
+              'type' => 'group',
+              'routeParams' => [
+                'userId' => $userData['id'],
+                'groupId' => $keycloakWisskiInstanceUserGroup['id'],
+              ],
+              'token' => $keycloakToken,
+            ]);
+            $keycloakMakeAddUserToGroupRequest = $this->sodaScsKeycloakServiceUserActions->makeRequest($keycloakBuildAddUserToGroupRequest);
 
-          if (!$keycloakMakeAddUserToGroupRequest['success']) {
-            throw new \Exception('Keycloak add user to admin group request failed: ' . $keycloakMakeAddUserToGroupRequest['error']);
-          }
-          break;
+            if (!$keycloakMakeAddUserToGroupRequest['success']) {
+              Error::logException(
+                $this->logger,
+                new \Exception('Keycloak add user to admin group request failed.'),
+                'Keycloak add user to admin group request failed. @message',
+                ['@message' => $keycloakMakeAddUserToGroupRequest['error']],
+                LogLevel::ERROR
+              );
+              $form_state->setErrorByName(
+                'connectedComponents',
+                $this->t('Keycloak add user to admin group request failed: @error', [
+                  '@error' => $keycloakMakeAddUserToGroupRequest['error'],
+                ]),
+              );
+            }
+            continue;
+
+          case 'soda_scs_sql_component':
+            $sqlComponent = $component;
+            $sqlMachineName = $sqlComponent->machineName->value;
+
+            // Grant all privileges for the database user.
+            $this->sodaScsSqlServiceActions->grantServiceRights($member->getDisplayName(), $sqlMachineName, ['ALL']);
+
+            continue;
+
+          case 'soda_scs_triplestore_component':
+            $triplestoreComponent = $component;
+            $triplestoreMachineName = $triplestoreComponent->machineName->value;
+            continue;
+
+          default:
+            $this->messenger()->addWarning($this->t('Component @component is not supported yet.', [
+              '@component' => $component->label(),
+            ]));
+            continue;
+        }
       }
     }
+  }
 
-    $form_state->setRedirect('entity.soda_scs_project.collection');
-
+  /**
+   * {@inheritdoc}
+   */
+  public function save(array $form, FormStateInterface $form_state) {
+    parent::save($form, $form_state);
+    $this->messenger()->addMessage($this->t('Project @project has been created.', [
+      '@project' => $this->entity->label(),
+    ]));
+    // Redirect to the project listing page.
+    $form_state->setRedirect('entity.soda_scs_project.canonical', ['soda_scs_project' => $this->entity->id()]);
   }
 
 }
